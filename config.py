c7dd4be1ea3e1620b2586dc2237f4eccc42f455b
flamhaze5946 <flamhaze5946@gmail.com>
2021-10-29 19:44:52 +0800

    rebase

--- config.py
+++ config.py
@@ -36,6 +36,7 @@ add_pos_amount = config_dict['trade']['add_pos_amount']  # 加仓次数，0不�
 kline_source = config_dict['trade']['kline_source']
 
 trade_klines_fetch_worker = config_dict['system']['redis_klines_web_fetch_worker']
+redis_namespace = config_dict['system']['redis_klines_namespace']
 
 redis_config = config_dict['system']['redis']
 redis_pool = redis.ConnectionPool(host=redis_config['host'], port=redis_config['port'],
